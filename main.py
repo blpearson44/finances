c92f16a9440de7830ed25ff3ba1fbdc59e5a17b5
Ben Pearson <blpearson44@icloud.com>
2020-11-28 16:47:24 -0800

    updated file names

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import csv
 
 # load contents of json file
 imported = {}
-with open("data1.json", "r") as f:
+with open("account-data.json", "r") as f:
     accounts = json.load(f)
 # instantiate contents as account objects
 for key, value in accounts.items():
@@ -25,18 +25,23 @@ with open(filename, "r") as csvfile:
     csvreader = csv.reader(csvfile)
     fields = next(csvreader)
     for row in csvreader:
-        rows.append([float(row[0]), float(row[4]), row[2], row[3]])
+        rows.append([float(row[0]), row[2], row[3], float(row[4])])
 
 # move transactions into local list
 print(fields)
 for x in rows:
-    trans = transaction.Transaction(x[2], x[0], x[1], x[3])
+    # kinda dont like the way the numbers on this one work, I'm probably going to come back and change it
+    trans = transaction.Transaction(x[0], x[1], x[2], x[3])
     transactions.append(trans)
 
 # this is to test that the transactions are in the list properly
 for x in transactions:
     x.printInfo()
 
+with open(filename, "w") as csvfile:
+    csvwriter = csv.writer(csvfile)
+    for row in transactions:
+        csvwriter.append()
 # Save contents of account objects to json file
-with open("data1.json", "w") as f:
-    json.dump(accounts, f, default=account.transform)
+# with open("account-data.json", "w") as f:
+#     json.dump(accounts, f, default=account.transform)
--- transaction.py
+++ transaction.py
@@ -2,11 +2,14 @@
 # I think it is probably just going to be equal to the delta of some date in the
 # past and a current date
 class Transaction:
-    def __init__(self, account=None, date=None, amount=None, category=None):
+    def __init__(self, date=None, account=None, category=None, amount=None):
         self.account = account
         self.date = date
         self.amount = amount
         self.category = category
+
+    def __repr__(self):
+        return [self.date, self.account, self.category, self.amount]
 
     def printInfo(self):
         print(self.account + "\t" + str(self.date) + "\t" +
